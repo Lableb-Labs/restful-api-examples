feca03cc87adc0146272349e565caccea0bbf513
lalosh <louayalosh17@gmail.com>
2020-12-08 12:57:29 +0200

    Add inital page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {'خدمة البحث داخل المواقع والتطبيقات لزيادة مبيعات، وتحسين تجربة المستخدم. للاستفسار معنا على salam@lableb.com'}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://solutions.lableb.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read full documentation @Lableb
         </a>
       </header>
     </div>
