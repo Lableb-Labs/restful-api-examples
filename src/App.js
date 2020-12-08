d13f99246db6174f1f127a217b54c6254df5426c
lalosh <louayalosh17@gmail.com>
2020-12-08 13:02:47 +0200

    Update logo src, and add React.js import statement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.png';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'https://solutions.lableb.com/logo.png'} className="App-logo" alt="logo" />
         <p>
           {'خدمة البحث داخل المواقع والتطبيقات لزيادة مبيعات، وتحسين تجربة المستخدم. للاستفسار معنا على salam@lableb.com'}
         </p>
