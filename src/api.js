0669a6e60522bfe3cd1d07a6601d49721cb92b10
lalosh <louayalosh17@gmail.com>
2021-04-26 13:24:18 +0300

    update token

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 export function deleteDocuments() {
 
-    const PROJECT_NAME = 'testperformance';
+    const PROJECT_NAME = 'restful-api-examples';
     const COLLECTION_NAME = 'posts';
-    const INDEXING_TOKEN = 'Mczn363962-P8Zab-fuwNHgBOll6FZlfE-F';
-    const TARGET_DOCUMENT_ID_TO_DELETE = 204879;
+    const INDEXING_TOKEN = 'kurwb7554701-snsyTRc6biMpHLKZziqVBU9l';
+    const TARGET_DOCUMENT_ID_TO_DELETE = 4;
 
 
     const queryParams = {
@@ -11,7 +11,7 @@ export function deleteDocuments() {
     };
 
 
-    const URL = `https://192.99.17.57:30001/api/v2/projects/${PROJECT_NAME}/collections/${COLLECTION_NAME}/documents/${TARGET_DOCUMENT_ID_TO_DELETE}?${new URLSearchParams(queryParams)}`;
+    const URL = `https://cse-prd.lableb.com/api/v2/projects/${PROJECT_NAME}/collections/${COLLECTION_NAME}/documents/${TARGET_DOCUMENT_ID_TO_DELETE}?${new URLSearchParams(queryParams)}`;
 
 
     return fetch(
